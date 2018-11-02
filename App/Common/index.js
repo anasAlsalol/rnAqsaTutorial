8806cc90c07d5182ddcc662c5a739a71c5a4c125
Anas Salool <anasalsalool@gmail.com>
2018-11-02 17:57:47 +0200

    SE6

---
+++ App/Common/index.js
@@ -0,0 +1,7 @@
+/*import reactotron from 'reactotron-react-native';
+
+export const log = values => reactotron.log(values);
+export const warn = values => reactotron.warn(values);
+export const error = values => reactotron.error(values);
+
+export const Reactotron = reactotron;*/
---
+++ App/examples/importing.js
@@ -0,0 +1,34 @@
+/*
+//
+// import defaultThing from './exporting'
+//
+//
+//
+//
+// import defaultThing as somethingElse from './exporting'
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// import defaultThing, {widget, birthdayReminder, shinyButton, trees} from './exporting'
+
+
+export function importing() {
+
+} */
